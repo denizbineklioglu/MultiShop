fb751d6bc42f43020f978084abd96803ed7bc873
Deniz Bineklioğlu <76698070+denizbineklioglu@users.noreply.github.com>
2024-12-30 00:21:00 +0300

    catalog service, product and category entities are created.

---
+++ Services/Catalog/MultiShop.Catalog/Entities/Category.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MultiShop.Catalog.Entities
+{
+	public class Category
+	{
+		// Mongo DB'nin ID'yi benzersiz değer görmesi için.
+		[BsonId]
+		[BsonRepresentation(BsonType.ObjectId)]
+		public string CategoryID { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
